906a5f0829b83d0cceb7131ea0f6e8749ec9c85d
sanha kang <ncsana@gmail.com>
2020-12-10 22:56:14 +0900

    앱 시작 시점에 위치 정보 요청 구현

--- FineDust/ViewController.swift
+++ FineDust/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
 
+
+    @IBOutlet weak var imageT: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        print("Start ViewController")
+//         Do any additional setup after loading the view.
+        
+        print("Requst Location Info")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
 
+        
+        print("Start animatedImages")
+        imageT.animationImages = animatedImages(for: "clear/tree")
+        imageT.animationDuration = 0.9
+        imageT.animationRepeatCount = 10
+        imageT.image = imageT.animationImages?.first
+        imageT.startAnimating()
+        
+        
+    }
+    
+    func animatedImages(for name: String) -> [UIImage] {
+        
+        var i = 1
+        var images = [UIImage]()
+        
+        
+        while let image = UIImage(named: "\(name)\(i)") {
+            images.append(image)
+            print("\(name)\(i)")
+            i += 1
+        }
+        return images
+    }
 
 }
 
